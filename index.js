f2a200178a35899b50fc033d7dae6c16b622b866
lyb <daochouwangu@gmail.com>
2021-07-17 20:47:50 +0800

    Update index.js
    加入link
    重新改回函数调用
    调用createTasks是新对象，后续链式调用均为同一个对象

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-(function createTasks() {
+function createTasks() {
   // 过滤不重要的
   function onlyRequired() {
-    const newObj = Object.assign({}, this)
-    newObj.articles = this.articles.filter((a) => a.is_required)
-    return newObj.copyToClipBoard()
+    this.articles = this.articles.filter((a) => a.is_required)
+    return this.copyToClipBoard()
   }
   // 过滤读过的
   function onlyUnread() {
-    const newObj = Object.assign({}, this)
-    newObj.articles = this.articles.filter((a) => !a.is_finished)
-    return newObj.copyToClipBoard()
+    this.articles = this.articles.filter((a) => !a.is_finished)
+    return this.copyToClipBoard()
   }
   /**
    *  markdown todo语法
    */
   function withMarkdown() {
-    const newObj = Object.assign({}, this)
-    newObj.articles = this.articles.map(a => {
-      const na = Object.assign({}, a)
-      if (na.is_finished) {
-        na.article_title = "- [x] " + na.article_title
+    if (this.isMarkdown) return this
+    this.isMarkdown = true
+    this.articles = this.articles.map(a => {
+      if (a.is_finished) {
+        a.prefix = "- [x] "
       } else {
-        na.article_title = "- [ ] " + na.article_title
+        a.prefix = "- [ ] "
       }
-      return na;
+      return a;
     })
-    return newObj.copyToClipBoard()
+    return this.copyToClipBoard()
   }
   /**
    * 带上计划时间参数
@@ -33,21 +31,34 @@
    * @returns 
    */
   function withTime(peerDay=2) {
+    if (this.isWithTime) return this
+    this.isWithTime = true
     const count = this.articles.filter(a => !a.is_finished).length
     const timeTextGen = getTimeTextGen(peerDay, count)();
-    const newObj = Object.assign({}, this)
-    newObj.articles = this.articles.map(a => {
-      const na = Object.assign({}, a)
-      if (!na.is_finished) {
-        na.article_title = na.article_title.replace('|', '｜') + '\t' + timeTextGen.next().value
+    this.articles = this.articles.map(a => {
+      if (!a.is_finished) {
+        a.article_title = a.article_title.replace('|', '｜')
+        a.suffix = '\t'+timeTextGen.next().value
+      } else {
+        a.suffix = ''
       }
-      return na;
+      return a;
     })
-    return newObj.copyToClipBoard()
+    return this.copyToClipBoard()
+  }
+  function withLink() {
+    if (this.isWithLink) return this
+    this.isWithLink = true
+    const prefix = 'https://time.geekbang.org/column/article/';
+    this.articles = this.articles.map(a => {
+      a.article_title = `[${a.article_title}](${prefix+a.id})`
+      return a
+    })
+    return this.copyToClipBoard()
   }
   function copyToClipBoard() {
     console.log("已经将所有文章标题复制到剪贴板， 可以直接粘贴到带批量生成todo的app中，比如滴答清单、微软todo")
-    copy(this.articles.map((a) => a.article_title).join('\n'))
+    copy(this.articles.map((a) => a.prefix + a.article_title + a.suffix +'  ').join('\n'))
     return this
   }
   
@@ -75,16 +86,22 @@
   }
 
   const $vm = document.querySelector("#app").children[0].__vue__
-  const articles = $vm.articleList || $vm.articles
+  let articles = $vm.articleList || $vm.articles
+  articles = articles.map(a => {
+    let na = Object.assign({}, a)
+    na.prefix = ''
+    na.suffix = ''
+    return na
+  })
   const result = {
     articles,
     withTime,
     onlyRequired,
     onlyUnread,
     copyToClipBoard,
-    withMarkdown
+    withMarkdown,
+    withLink
   }
-  window.result = result
   result.copyToClipBoard()
   console.log("已经将所有文章标题复制到剪贴板， 可以直接粘贴到带批量生成todo的app中，比如滴答清单、微软todo")
   console.log(`接下来使用进行过滤和增加时间限制的高级功能
@@ -92,7 +109,9 @@
     result.onlyUnread来过滤已经读过的
     result.withTime 生成带时间限制的todo,要传入一个每天读几章的参数，默认2
     result.withMarkdown 生成支持markdown todo的文本
+    result.withLink  生成带链接的todo
     支持链式调用，比如
     result.onlyUnread().withTime()`
   )
-})()
+  return result
+}
