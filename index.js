55336412f8c435ba0b2a44b8dea4285fc8884e9a
lyb <daochouwangu@gmail.com>
2021-07-17 18:29:44 +0800

    第二版
    支持链式调用
    支持自动判断未读、非重要

--- index.js
+++ index.js
@@ -2,23 +2,31 @@
   /**
    * 
    * @param {每天阅读文章数量} peerDay 
-   * @param {已经阅读的文章数量} startOffset 
+   * @param {只阅读正文} onlyRequired 
    * @returns 
    */
-  function start(peerDay=2, startOffset = 0) {
-    if (startOffset < 0) {
-      throw Error('已学数量不能为负数')
-    }
-    if (startOffset >= result.length) {
-      throw Error(`已学数量过长, 范围：[0-${result.length-1}]`)
-    }
-    const left = result.length - startOffset
-    const timeTextGen = getTimeTextGen(peerDay, left)();
-    const tasks = result.slice(0, startOffset).join('\n')
-                + (startOffset > 0 ? '\n' : '')
-                + result.slice(startOffset).map((article, i) => article + '\t' + timeTextGen.next().value).join('\n')
-    copy(tasks)
-    console.log('已经复制到粘贴板')
+  function withTime(peerDay=2) {
+    const count = this.articles.filter(a => !a.is_finished).length
+    const timeTextGen = getTimeTextGen(peerDay, count)();
+    const newObj = Object.assign({}, this)
+    newObj.articles = this.articles.map(a => {
+      const na = Object.assign({}, a)
+      if (!na.is_finished) {
+        na.article_title += '\t' + timeTextGen.next().value
+      }
+      return na;
+    })
+    return newObj.copyToClipBoard()
+  }
+  function onlyRequired() {
+    const newObj = Object.assign({}, this)
+    newObj.articles = this.articles.filter((a) => a.is_required)
+    return newObj.copyToClipBoard()
+  }
+  function onlyUnread() {
+    const newObj = Object.assign({}, this)
+    newObj.articles = this.articles.filter((a) => !a.is_finished)
+    return newObj.copyToClipBoard()
   }
   function getTimeTextGen(peerDay, count) {
     const today = Date.now();
@@ -42,14 +50,26 @@
     const date = new Date(time)
     return `${date.getMonth()+1}-${date.getDate()}`
   }
-  const result = []
-  document.querySelectorAll("a[data-seo]").forEach((article) => {
-    result.push(article.innerText.replace('|', ''))
-  })
-  const text = result.join('\n')
-  copy(text)
-  console.log("已经将文章列表复制到剪贴板")
-  console.log("接下来可以通过调用start(peerDay=2, startOffset=0)方法传入每天学习文章数量(>=1,整数)和已经学过的文章数量，自动生成带时间的任务列表")
-  window.start = start
+  function copyToClipBoard() {
+    copy(this.articles.map((a) => a.article_title).join('\n'))
+    return this
+  }
+  const articles = document.querySelector("#app").children[0].__vue__.articleList
+  const result = {
+    articles,
+    withTime,
+    onlyRequired,
+    onlyUnread,
+    copyToClipBoard
+  }
+  window.result = result
+  result.copyToClipBoard()
+  console.log("已经将所有文章标题复制到剪贴板， 可以直接粘贴到带批量生成todo的app中，比如滴答清单、微软todo")
+  console.log(`接下来使用进行过滤和增加时间限制的高级功能
+    result.onlyRequired 来过滤非重要的，比如答疑结尾语
+    result.onlyUnread来过滤已经读过的
+    result.withTime 生成带时间限制的todo,要传入一个每天读几章的参数，默认2
+    支持链式调用，比如
+    result.onlyUnread().withTime()`
+  )
 })()
-
